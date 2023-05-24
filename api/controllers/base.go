9286504a2e0ca38903fadcf9d31e49edb02c163b
mazeko <Eko.Purnomo@sera.astra.co.id>
2023-05-24 11:47:33 +0700

    ver:1.0.4 msg:create base controller - initial server

---
+++ api/controllers/base.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres database driver
+
+	"github.com/mazeko/go-blog/api/models"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize(DbDriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+	var err error
+	if DbDriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+		server.DB, err = gorm.Open(DbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s Database", DbDriver)
+			log.Fatal("This is error: ", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", DbDriver)
+		}
+	}
+
+	if DbDriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+		server.DB, err = gorm.Open(DbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s Database", DbDriver)
+			log.Fatal("This is error: ", err)
+		} else {
+			fmt.Printf("We are connected to the %s database", DbDriver)
+		}
+	}
+
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) //database migration
+	server.Router = mux.NewRouter()
+	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("listening to port 8080")
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
